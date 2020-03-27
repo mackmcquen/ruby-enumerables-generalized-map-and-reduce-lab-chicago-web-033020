7a0b1a4f6bd13c3f3d0dc77d4c927a7f674e3335
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 21:09:42 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  array1 = []
+  counter = 0
+  while counter < array.size do
+    array1.push(yield(array[counter]))
+    counter += 1
+end
+array1
+end
+
+def reduce(array, starting_point=nil)
+  if starting_point
+    array1 = starting_point
+    counter = 0
+  else
+    array1 = array[0]
+    counter = 1
+end
+  while counter < array.length
+    array1 = yield(array1, array[counter])
+    counter += 1
+end
+array1
+end
